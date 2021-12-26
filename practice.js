7cd9551b3cbdcd448d22dca5d9eb88cf36991a10
Ayaan R <53174223+astronomerayaan@users.noreply.github.com>
2021-12-26 10:24:32 -0800

    firebaseinit2

--- practice.js
+++ practice.js
@@ -1,4 +1,6 @@
 function addUser(){
   var user_name = document.getElementById("user_name").value;
-  firebase.database().ref("/").child(user_name).update({});
+  firebase.database().ref("/").child(user_name).update({
+    
+  });
 }
