52e6d8a96e3a4a153e5f6a021eb693f286fb4781
Ayaan R <53174223+astronomerayaan@users.noreply.github.com>
2021-12-26 10:26:34 -0800

    firebaseinit3

--- practice.js
+++ practice.js
@@ -1,6 +1,16 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyCXzYWuadEf-KxET5KebXi3uXTgSdaM3VM",
+  authDomain: "kwitter-caad5.firebaseapp.com",
+  databaseURL: "https://kwitter-caad5-default-rtdb.firebaseio.com",
+  projectId: "kwitter-caad5",
+  storageBucket: "kwitter-caad5.appspot.com",
+  messagingSenderId: "827196602785",
+  appId: "1:827196602785:web:2b8933be6c7326b9da33e0"
+};
+firebase.initializeApp(firebaseConfig);
 function addUser(){
   var user_name = document.getElementById("user_name").value;
   firebase.database().ref("/").child(user_name).update({
-    
+    purpose: "Hi I am Ayaan"
   });
 }
